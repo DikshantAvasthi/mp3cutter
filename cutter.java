b4c5e3f0080b9ee3a0ddc5008a9a2626ee9a6893
DikshantAvasthi <42887577+DikshantAvasthi@users.noreply.github.com>
2018-10-15 18:49:38 +0530

    Update cutter.java

--- cutter.java
+++ cutter.java
@@ -37,33 +37,5 @@ catch (Exception e)
 	System.out.println(e);
 }
 
-
-try {
-File g=new File("C:\\Users\\Dikshant Avasthi\\Desktop\\addicted.mp3");
-File k=new File("C:\\Users\\Dikshant Avasthi\\Desktop\\laung.mp3");
-FileInputStream tis= new FileInputStream(g);
-FileInputStream pis= new FileInputStream(k);
-FileOutputStream fos3=new FileOutputStream("C:\\Users\\Dikshant Avasthi\\Desktop\\koina.mp3");
-int q=0;
-int w=0;
-int l1 = (int)g.length();
-int l2 = (int)k.length();
-while ( q < l1)
-{
-	fos3.write(tis.read());
-	q++;
-}
-
-while (w < l2)
-{
-	fos3.write(pis.read());
-     w++;
-	}
-
-}
-catch (Exception e)
-{
-	System.out.println(e);
-}
 }
 }
